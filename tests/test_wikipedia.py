e2948efd522aac3b267265c833e50b9e905a9c01
Evgeny Kirillov <evgeny.kirillov@alarislabs.com>
2022-12-07 09:20:26 +0700

    part 2

---
+++ tests/test_wikipedia.py
@@ -0,0 +1,10 @@
+import pytest
+from hypermodern_python_tutorial import wikipedia
+
+
+def test_random_page_uses_given_language(mock_requests_get):
+    wikipedia.random_page(language="de")
+    args, _ = mock_requests_get.call_args
+    assert "de.wikipedia.org" in args[0]
+
+
